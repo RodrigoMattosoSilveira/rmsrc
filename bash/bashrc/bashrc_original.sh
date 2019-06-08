42f325134e1b47b42866c4b1c28a764a47c6e162
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-06-08 10:06:50 -0700

    master  -  Fixed a typo

--- bash/bashrc/bashrc_original.sh
+++ bash/bashrc/bashrc_original.sh
@@ -1,4 +1,4 @@
-https://www.thegeekstuff.com/2008/09/bash-shell-ps1-10-examples-to-make-your-linux-prompt-like-angelina-jolie/
+#https://www.thegeekstuff.com/2008/09/bash-shell-ps1-10-examples-to-make-your-linux-prompt-like-angelina-jolie/
 # http://bashrcgenerator.com/
 #http://ezprompt.net/
 export PS1="\[\e[36m\]\u\[\e[m\]@\[\e[32m\]\h\[\e[m\][\[\e[33m\]\w\[\e[m\]]\n$ "
