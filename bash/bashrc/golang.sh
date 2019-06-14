01329d16934e95d852779a039a04666cce65f500
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-06-14 08:13:25 -0700

    master  -  Back to go

--- bash/bashrc/golang.sh
+++ bash/bashrc/golang.sh
@@ -1,4 +1,4 @@
 PATH=$PATH:/usr/local/go/bin
 PATH=$PATH:$HOME/projects/go/bin
 export GOPATH=$HOME/projects/go
-alias togo='cd ~/projects/theGoProgramingLanguage'
+alias togo='cd ~/projects/go/src'
