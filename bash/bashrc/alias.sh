cbf6635cff6b56881e39f79b1572f677d03a6ae7
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-06-14 08:00:04 -0700

    master  -  Refactored, documented

--- bash/bashrc/alias.sh
+++ bash/bashrc/alias.sh
@@ -1,7 +1,7 @@
 alias ll='ls -alF'
 
+# Note that some alias commands might be with the Language, e.g, g0
 alias tobash='source ~/.bashrc'
-alias togo='~/projects/theGoProgramingLanguage'
 alias tongw='cd ~/projects/ng-workspace'
 alias toprj='cd ~/projects'
 alias tosl='cd ~/projects/rms-sparklines'
