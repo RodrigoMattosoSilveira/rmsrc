94453ecde13b712f7cd8b4eebd41eaa3240d2b39
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-06-16 08:42:58 -0700

    master  -  Added cat of the alias and path scripts, and an alias to navigate to this repo!

--- bash/bashrc/alias.sh
+++ bash/bashrc/alias.sh
@@ -1,8 +1,14 @@
+#!/usr/bin/env bash
+
 alias ll='ls -alF'
 
-# Note that some alias commands might be with the Language, e.g, g0
+# Note that some alias commands might be with the Language, e.g, go
+alias catalias='cat ~/rmsrc/bash/bashrc/alias.sh'
+alias catpath='cat ~/rmsrc/bash/bashrc/path.sh'
+
 alias tobash='source ~/.bashrc'
 alias tongw='cd ~/projects/ng-workspace'
 alias toprj='cd ~/projects'
+alias tormsrc='cd ~/rmsrc'
 alias tosl='cd ~/projects/rms-sparklines'
 alias towuf='cd ~/projects/wuf'
