709566890ea9f635d008c5d88ddd06e7e3397656
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-09-13 14:10:27 -0700

    master  -  Add path to ruby and its gems

--- bash/bashrc/path.sh
+++ bash/bashrc/path.sh
@@ -1,5 +1,8 @@
 PATH=/Applications
-PATH=$PATH:/bin:/usr/bin
+PATH=$PATH:/bin
+PATH=$PATH:/usr/local/Cellar/ruby/2.6.4_2/bin
+PATH=$PATH:/usr/local/lib/ruby/gems/2.6.0/bin
+PATH=$PATH:/usr/bin
 PATH=$PATH:/usr/local/bin
 PATH=$PATH:/usr/sbin:/sbin
 PATH=$PATH:~/rmsrc/bash/bin
