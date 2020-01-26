9f32b9264ce6b7c04c5203dfb624b8064e729fe9
Rodrigo Silveira <r3850365@gmail.com>
2020-01-26 15:56:08 -0800

    master  -  Added a path to the NLTK dataset

--- bash/bashrc/pyMLByExample.sh
+++ bash/bashrc/pyMLByExample.sh
@@ -4,8 +4,10 @@
 set -e
 
 if [ -r ~/projects/pyMlByExample ]; then
-   echo ".bashrc: cd ~/projects/pyMlByExample"
+   echo "pyMlByExample.sh: cd ~/projects/pyMlByExample"
    cd ~/projects/pyMlByExample
-   echo ".bashrc: source ~/projects/pyMlByExample/bin/activate"
+   echo "pyMlByExample.sh: source ~/projects/pyMlByExample/bin/activate"
    source ~/projects/pyMlByExample/bin/activate
+   echo "pyMlByExample.sh: export NLTK_DATA=~/nltk_data:$PATH"
+   export NLTK_DATA=~/nltk_data:$PATH
 fi
