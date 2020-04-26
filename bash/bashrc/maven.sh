3857dd7f730a7ac022de02ea9a48878c053e42ce
Rodrigo Silveira <r3850365@gmail.com>
2020-04-26 09:04:06 -0700

    master  -  refactor: Added support for Maven 3.6.3

--- bash/bashrc/maven.sh
+++ bash/bashrc/maven.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export M2_HOME="~/Applications/apache-maven-3.6.1"
+export M2_HOME="/Applications/apache-maven-3.6.3"
 export PATH=${M2_HOME}/bin:$PATH
 export MAVEN_OPTS="$MAVEN_OPTS -Djava.nio.channels.spi.SelectorProvider=sun.nio.ch.KQueueSelectorProvider"
