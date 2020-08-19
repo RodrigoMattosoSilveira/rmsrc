5eb3c38b80533795bf8db74ec0df1691b63ea327
Rodrigo Silveira <r3850365@gmail.com>
2020-08-19 16:19:34 -0700

    master  -  refactor: installed OPENJDK 14

--- bash/bashrc/java.sh
+++ bash/bashrc/java.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-13.0.2.jdk/Contents/Home"
+#export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-13.0.2.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-14.jdk/Contents/Home"
+PATH=$PATH:$JAVA_HOME/bin;
+
+
