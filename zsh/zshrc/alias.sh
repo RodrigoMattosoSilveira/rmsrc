828dd900c22f9cb760fef823cbf3ad66d508b8c6
Rodrigo Silveira <r3850365@gmail.com>
2021-06-06 13:18:15 -0700

    Added a path alias, and a configured the prompt.

--- zsh/zshrc/alias.sh
+++ zsh/zshrc/alias.sh
@@ -4,6 +4,7 @@ echo "sourcing /Users/rodrigosilveira/rmsrc/zsh/zshrc/alias.sh"
 
 # command line aliases
 alias ll='ls -alF'
+alias path='env | grep "PATH"'
 
 # CAT aliases to see what is in certain files
 alias catalias='cat ~/rmsrc/zsh/zshrc/alias.sh'
