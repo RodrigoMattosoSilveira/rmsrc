21cf39fbec2061646a32013c33340e8bea587057
Rodrigo Silveira <rodrigo.silveira@ohi.com>
2021-12-16 13:23:07 -0800

    Replaced python@3.8 with python@3.10

--- zsh/zshenv/path.sh
+++ zsh/zshenv/path.sh
@@ -3,4 +3,4 @@ echo "sourcing /Users/rodrigosilveira/rmsrc/zsh/zshenv/path.sh"
 
 PATH=$PATH:/Applications
 PATH=$PATH:~/.local/bin
-PATH=$PATH:/Users/ohitech/Library/Python/3.8/bin
+#PATH=$PATH:/Users/ohitech/Library/Python/3.8/bin
